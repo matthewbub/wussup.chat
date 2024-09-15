0ae8d791ad62e77920b8065f1b6263670d6644ee
matéo! <6matbub@gmail.com>
2024-09-15 00:09:45 -0700

    update git ignore

---
+++ db.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func db() *sql.DB {
+	db, err := sql.Open("sqlite3", "./cmd/dbm/dev.db")
+	if err != nil {
+		fmt.Println("Error opening database. Are you sure it exists?")
+		log.Fatal(err)
+	}
+	return db
+}
