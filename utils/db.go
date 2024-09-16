0ef09912169b53b364cf35dc794d0c8596d3f8d0
matéo! <6matbub@gmail.com>
2024-09-15 23:35:24 -0700

    fix broken path to db

--- utils/db.go
+++ utils/db.go
@@ -4,12 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func Db() *sql.DB {
-	db, err := sql.Open("sqlite3", "../cmd/dbm/dev.db")
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting current working directory:", err)
+	}
+
+	// Construct the database path
+	dbPath := filepath.Join(cwd, "cmd", "dbm", "dev.db")
+
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		fmt.Println("Error opening database. Are you sure it exists?")
 		log.Fatal(err)
