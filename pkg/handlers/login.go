b1b5fa575f28050cb4f1272b8a454622121e18b6
matéo! <6matbub@gmail.com>
2024-10-25 21:33:33 -0700

    clean up login view handler
    aka make it easier to debug cause its hella annoying being logged in and still being able to access the login form

--- pkg/handlers/login.go
+++ pkg/handlers/login.go
@@ -1,18 +1,24 @@
 package handlers
 
 import (
+	"net/http"
+
+	"bus.zcauldron.com/pkg/utils"
 	"bus.zcauldron.com/pkg/views"
 	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 )
 
 func LoginView(c *gin.Context) {
-	// TODO improve user auth behavior
-	// If logged in, redirect to dashboard
+	session := utils.GetSession(c)
+	userID := session.Get("user_id")
+	if userID != nil {
+		c.Redirect(http.StatusSeeOther, "/dashboard")
+		return
+	}
+
 	templ.Handler(views.LogIn(views.LogInData{
 		Title:      "Login",
-		Name:       "World",
 		IsLoggedIn: false,
-		Message:    "Welcome to the login page",
 	})).ServeHTTP(c.Writer, c.Request)
 }
