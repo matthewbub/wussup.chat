f5d36b3665653e715e731f1b39cb5d45e6f98dcf
matéo! <6matbub@gmail.com>
2024-11-02 00:20:23 -0700

    wrap admin screen in auth

--- routes/src/routes/index.tsx
+++ routes/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { fetchSecureTest } from "@/utils/auth-helpers";
 import { useAuthStore } from "@/stores/auth";
 import { Button } from "@/components/ui/button";
-
+import { Authorized } from "@/components/Authorized";
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
@@ -22,50 +22,52 @@ function HomeComponent() {
   }, []);
 
   return (
-    <div className="p-2 mx-auto max-w-2xl">
-      <h1 className="text-3xl font-bold pb-4">Debugger</h1>
-      <ol>
-        <li>
-          <strong>Authenticated:</strong>{" "}
-          {isAuthenticated ? "Logged in" : "Logged out"}
-        </li>
-        <li>
-          <strong>Security questions answered:</strong>{" "}
-          {isSecurityQuestionsAnswered ? "Yes" : "No"}
-          {isAuthenticated && !isSecurityQuestionsAnswered && (
-            <a
-              href="/security-questions"
-              className="pl-1 text-blue-500 dark:text-blue-400 hover:underline"
-            >
-              Answer security questions
-            </a>
-          )}
-        </li>
-        {user && (
+    <Authorized>
+      <div className="p-2 mx-auto max-w-2xl">
+        <h1 className="text-3xl font-bold pb-4">Debugger</h1>
+        <ol>
           <li>
-            <strong>User:</strong>
-            <ul className="pl-4">
-              <li>ID: {user.id}</li>
-              <li>Username: {user.username}</li>
-              <li>Email: {user.email}</li>
-            </ul>
+            <strong>Authenticated:</strong>{" "}
+            {isAuthenticated ? "Logged in" : "Logged out"}
           </li>
-        )}
-      </ol>
+          <li>
+            <strong>Security questions answered:</strong>{" "}
+            {isSecurityQuestionsAnswered ? "Yes" : "No"}
+            {isAuthenticated && !isSecurityQuestionsAnswered && (
+              <a
+                href="/security-questions"
+                className="pl-1 text-blue-500 dark:text-blue-400 hover:underline"
+              >
+                Answer security questions
+              </a>
+            )}
+          </li>
+          {user && (
+            <li>
+              <strong>User:</strong>
+              <ul className="pl-4">
+                <li>ID: {user.id}</li>
+                <li>Username: {user.username}</li>
+                <li>Email: {user.email}</li>
+              </ul>
+            </li>
+          )}
+        </ol>
 
-      {isAuthenticated && (
-        <div className="flex space-x-2 mt-4">
-          <Button onClick={fetchSecureTest}>Test Example</Button>
-          <Button
-            onClick={async () => {
-              await useLogout();
-            }}
-          >
-            Logout
-          </Button>
-          <Link href="/account/reset-password">Reset Password</Link>
-        </div>
-      )}
-    </div>
+        {isAuthenticated && (
+          <div className="flex space-x-2 mt-4">
+            <Button onClick={fetchSecureTest}>Test Example</Button>
+            <Button
+              onClick={async () => {
+                await useLogout();
+              }}
+            >
+              Logout
+            </Button>
+            <Link href="/account/reset-password">Reset Password</Link>
+          </div>
+        )}
+      </div>
+    </Authorized>
   );
 }
