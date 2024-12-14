280a62a99ee64a1db1d9a667d0d1bc950d1227d6
matéo! <6matbub@gmail.com>
2024-12-14 06:17:17 -0800

    add hono

---
+++ lib/auth/readme.md
@@ -0,0 +1,3 @@
+# Auth system
+
+We're using Hono as an HTTP framework, and hosting on Cloudflare workers. First time here? Watch this vid: https://www.youtube.com/watch?v=H7Qe96fqg1M to get up to speed on Cloudflare Workers and Hono.
--- lib/auth/src/index.ts
+++ lib/auth/src/index.ts
@@ -1,23 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		console.log(JSON.stringify(request.cf));
-		return new Response(JSON.stringify({ message: 'Hola, Mundo!' }), {
-			headers: {
-				'content-type': 'application/json',
-			},
-		});
-	},
-} satisfies ExportedHandler<Env>;
+export interface Env {}
+
+const app = new Hono<{ Bindings: Env }>();
+
+// middleware
+app.use(logger());
+
+// routes
+app.get('/', (c) => {
+	return c.text('Hello World');
+});
+
+export default app;
