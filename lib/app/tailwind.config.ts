84b5ae7ae8076ccca3c32532ff66feb193902893
matéo! <6matbub@gmail.com>
2025-01-11 14:55:36 -0800

    more styles!

--- lib/app/tailwind.config.ts
+++ lib/app/tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
 
 export default {
   content: [
@@ -7,16 +6,5 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
+  plugins: [],
 } satisfies Config;
