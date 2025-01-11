20fbcbc5199e98b68fe0eeb962e4637d005ec450
matéo! <6matbub@gmail.com>
2025-01-11 11:31:16 -0800

    deploy mvp of auth package

--- lib/app/src/app/login/page.tsx
+++ lib/app/src/app/login/page.tsx
@@ -1,11 +1,14 @@
-import Login from "@/components/system/Login";
+"use client";
+import Login from "@ninembs-studio/auth-ui/login";
 import PublicHeader from "@/components/system/PublicHeader";
+import { useRouter } from "next/router";
 
 export default function LoginPage() {
+  const router = useRouter();
   return (
     <div>
       <PublicHeader />
-      <Login />
+      <Login history={router} />
     </div>
   );
 }
