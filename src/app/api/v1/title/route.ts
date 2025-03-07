95d1a5ac913908691754f3f94400764aac21668e
matéo! <6matbub@gmail.com>
2025-03-06 20:10:43 -0800

    fix supabase issues in title gen

--- src/app/api/v1/title/route.ts
+++ src/app/api/v1/title/route.ts
@@ -11,11 +11,16 @@ export async function POST(req: Request) {
   const { messages, session_id } = await req.json();
 
   const supabase = await createClient();
+  const { data: userData } = await supabase.auth.getUser();
+
+  if (!userData.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
   // Take only first two messages for title generation
   const titleMessages = messages.slice(0, 2);
 
-  const { text, ...rest } = await generateText({
+  const { text } = await generateText({
     model: openai("gpt-4-turbo"),
     prompt: clsx([
       "Summarize the chat in a concise title using up to 6 words. Text only, no special characters. If the chat is empty, use a funny Futurama quote. For example: 'Shut up and take my money!' or 'Good news everyone!'",
@@ -24,13 +29,23 @@ export async function POST(req: Request) {
   });
 
   // update session title
-  const { error } = await supabase.from("ChatBot_Sessions").upsert({ name: text }).eq("id", session_id);
+  const { error } = await supabase.from("ChatBot_Sessions").upsert(
+    {
+      name: text,
+      user_id: userData.user.id,
+      updated_at: new Date(),
+      id: session_id,
+    },
+    {
+      onConflict: "id",
+    }
+  );
 
   if (error) {
     console.error("[title] error updating session title:", error);
   }
 
   console.log("[title] text", text);
-  console.log("[title] rest", rest);
+  // console.log("[title] rest", rest);
   return NextResponse.json({ title: text });
 }
