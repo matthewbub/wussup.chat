3fb035bad073f5b422efb27a8d2ff8d665de44e0
matéo! <6matbub@gmail.com>
2025-03-11 01:01:37 -0700

    dark theme on clerk components

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <div className="flex-1 flex justify-center items-center">
-        <SignUp />
+        <SignUp appearance={{ baseTheme: dark }} />
       </div>
       <Footer />
     </div>
