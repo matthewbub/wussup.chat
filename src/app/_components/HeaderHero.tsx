23acf582947d05e176dd3cc0287ea3cb19598ff0
matéo! <6matbub@gmail.com>
2025-03-13 15:39:30 -0700

    remove mobile nav we don't need it rn

--- src/app/_components/HeaderHero.tsx
+++ src/app/_components/HeaderHero.tsx
@@ -14,11 +14,7 @@ import { SignedIn } from "@clerk/nextjs";
 import { SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
-export function HeaderHero({ setActiveModal }: { setActiveModal: (modal: string) => void }) {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-  const router = useRouter();
-
+export function HeaderHero() {
   return (
     <div className="">
       <header className="absolute inset-x-0 top-0 z-50">
@@ -29,17 +25,6 @@ export function HeaderHero({ setActiveModal }: { setActiveModal: (modal: string)
               <Image alt="" src="/wussup.png" className="h-8 w-auto" width={32} height={32} />
             </a>
           </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              aria-label="Open main menu"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {/* {navigation.map((item) => (
               <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-white">
@@ -47,7 +32,7 @@ export function HeaderHero({ setActiveModal }: { setActiveModal: (modal: string)
               </a>
             ))} */}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4">
+          <div className="flex flex-1 justify-end gap-4">
             <SignedOut>
               <SignInButton>
                 <Button variant="ghost">Sign In</Button>
@@ -60,55 +45,10 @@ export function HeaderHero({ setActiveModal }: { setActiveModal: (modal: string)
               <Button variant="ghost" asChild>
                 <Link href="/~">Chat</Link>
               </Button>
-              <UserButton afterSignOutUrl="/" />
+              <UserButton />
             </SignedIn>
           </div>
         </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Wussup Chat</span>
-                <Image alt="" src="/wussup.png" className="h-8 w-auto" width={32} height={32} />
-              </a>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-400"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/25">
-                {/* <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-800"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div> */}
-                <div className="py-6">
-                  {isLoggedIn ? (
-                    <Button variant="outline" asChild onClick={() => router.push("/~")}>
-                      Dashboard
-                    </Button>
-                  ) : (
-                    <Button variant="outline" onClick={() => setActiveModal("auth")}>
-                      Log in
-                    </Button>
-                  )}
-                </div>
-              </div>
-            </div>
-          </DialogPanel>
-        </Dialog>
       </header>
 
       <div className="relative isolate pt-14">
