6ef15be5b5a28f9c5cbe482c70e5297da4b6d966
matéo! <6matbub@gmail.com>
2025-04-04 22:00:50 -0700

    use better typography

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -83,7 +83,7 @@ const DialogDescription = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Description ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
+  <DialogPrimitive.Description ref={ref} className={cn("text-body text-muted-foreground", className)} {...props} />
 ));
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
