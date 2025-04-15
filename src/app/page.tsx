553d1e9e8cdf0a7b4f170c812b2e606babf09f61
matéo! <6matbub@gmail.com>
2025-04-14 20:58:39 -0700

    rm try catch from app root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,101 +7,96 @@ import * as Sentry from "@sentry/nextjs";
 import { redirect } from "next/navigation";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ session?: string }> }) {
-  try {
-    const session = (await searchParams).session;
-    const { userId } = await auth();
+  const session = (await searchParams).session;
+  const { userId } = await auth();
 
-    if (!userId) {
-      redirect("/sign-in");
-    }
-
-    // Query for threads and the current threads messages, if applicable
-    const [threads, messages] = await Promise.all([
-      prisma.thread
-        .findMany({
-          where: {
-            userId: userId as string,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-        })
-        .catch((error) => {
-          Sentry.captureException(error);
-          return [];
-        }),
-      session
-        ? prisma.message
-            .findMany({
-              where: {
-                userId: userId as string,
-                threadId: session as string,
-              },
-            })
-            .catch((error) => {
-              Sentry.captureException(error);
-              return null;
-            })
-        : null,
-    ]);
-
-    void updateUserMetadataIfNeeded(userId).catch((error) => {
-      Sentry.captureException(error);
-    });
-
-    const isSubscribed = await isUserSubscribed(userId).catch((error) => {
-      Sentry.captureException(error);
-      return {
-        isSubscribed: false,
-        currentPeriodEnd: null,
-        currentPeriodStart: null,
-      };
-    });
-
-    const formattedThreads = threads?.map(
-      (thread: { id: string; createdAt: Date; updatedAt: Date; userId: string; name: string }) => {
-        try {
-          return {
-            ...thread,
-            created_at: new Date(thread.createdAt).toISOString(),
-            updated_at: new Date(thread.updatedAt).toISOString(),
-            chat_history: thread
-              ? messages
-                  ?.filter((message: { threadId: string }) => message.threadId === thread.id)
-                  .reduce((acc: { role: string; content: string }[], message: { input: string; output: string }) => {
-                    acc.push({
-                      role: "user",
-                      content: message.input,
-                    });
-                    acc.push({
-                      role: "assistant",
-                      content: message.output,
-                    });
-                    return acc;
-                  }, []) || []
-              : [],
-          };
-        } catch (error) {
-          Sentry.captureException(error);
-          return {
-            ...thread,
-            created_at: new Date(thread.createdAt).toISOString(),
-            updated_at: new Date(thread.updatedAt).toISOString(),
-            chat_history: [],
-          };
-        }
-      }
-    );
-
-    return (
-      <SessionWrapper existingData={formattedThreads || []} isSubscribed={isSubscribed}>
-        <ChatApp />
-      </SessionWrapper>
-    );
-  } catch (error) {
-    Sentry.captureException(error);
-    return <div>Something went wrong. Our team has been notified.</div>;
+  if (!userId) {
+    redirect("/sign-in");
   }
+
+  // Query for threads and the current threads messages, if applicable
+  const [threads, messages] = await Promise.all([
+    prisma.thread
+      .findMany({
+        where: {
+          userId: userId as string,
+        },
+        orderBy: {
+          updatedAt: "desc",
+        },
+      })
+      .catch((error) => {
+        Sentry.captureException(error);
+        return [];
+      }),
+    session
+      ? prisma.message
+          .findMany({
+            where: {
+              userId: userId as string,
+              threadId: session as string,
+            },
+          })
+          .catch((error) => {
+            Sentry.captureException(error);
+            return null;
+          })
+      : null,
+  ]);
+
+  void updateUserMetadataIfNeeded(userId).catch((error) => {
+    Sentry.captureException(error);
+  });
+
+  const isSubscribed = await isUserSubscribed(userId).catch((error) => {
+    Sentry.captureException(error);
+    return {
+      isSubscribed: false,
+      currentPeriodEnd: null,
+      currentPeriodStart: null,
+    };
+  });
+
+  const formattedThreads = threads?.map(
+    (thread: { id: string; createdAt: Date; updatedAt: Date; userId: string; name: string }) => {
+      try {
+        return {
+          ...thread,
+          created_at: new Date(thread.createdAt).toISOString(),
+          updated_at: new Date(thread.updatedAt).toISOString(),
+          chat_history: thread
+            ? messages
+                ?.filter((message: { threadId: string }) => message.threadId === thread.id)
+                .reduce((acc: { role: string; content: string }[], message: { input: string; output: string }) => {
+                  acc.push({
+                    role: "user",
+                    content: message.input,
+                  });
+                  acc.push({
+                    role: "assistant",
+                    content: message.output,
+                  });
+                  return acc;
+                }, []) || []
+            : [],
+        };
+      } catch (error) {
+        Sentry.captureException(error);
+        return {
+          ...thread,
+          created_at: new Date(thread.createdAt).toISOString(),
+          updated_at: new Date(thread.updatedAt).toISOString(),
+          chat_history: [],
+        };
+      }
+    }
+  );
+
+  return (
+    <SessionWrapper existingData={formattedThreads || []} isSubscribed={isSubscribed}>
+      <ChatApp />
+    </SessionWrapper>
+  );
 }
 
 async function updateUserMetadataIfNeeded(userId: string) {
